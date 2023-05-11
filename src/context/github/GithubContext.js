25476235eca13d7e0afa43ad60bd1bdb0b1b7500
Joshua Sauter <joshua.sauter@duke.edu>
2023-05-10 21:16:15 -0400

    Added alerts and user pages

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,11 +9,14 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_FINDER_TOKEN
 export const GithubProvider = ({ children }) => {
   const initialState ={
     users: [],
+    user: {},
+    repos: [],
     loading: false,
   } 
 
   const [state, dispatch] = useReducer(githubReducer, initialState)
 
+  // Get search results
   const searchUsers = async (text) => {
     setLoading()
 
@@ -35,6 +38,51 @@ export const GithubProvider = ({ children }) => {
     })
   }
 
+  // Get single user
+  const getUser = async (login) => {
+    setLoading()
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`
+      }
+    })
+
+    if (response.status === 404) {
+      window.location = '/notfound'
+    } else {
+      const data = await response.json()
+
+      dispatch({
+        type: 'GET_USER',
+        payload: data,
+      })
+    }
+  }
+
+  // Get user repos
+  const getUserRepos = async (login) => {
+    setLoading()
+
+    const params = new URLSearchParams({
+      sort: 'created',
+      per_page: 10
+    })
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`
+      }
+    })
+
+    const data = await response.json()
+
+    dispatch({
+      type: 'GET_REPOS',
+      payload: data,
+    })
+  }
+
   const clearUsers = () => dispatch({
     type: 'CLEAR_USERS'
   })
@@ -47,8 +95,12 @@ export const GithubProvider = ({ children }) => {
     <GithubContext.Provider value={{
       users: state.users,
       loading: state.loading,
+      user: state.user,
+      repos: state.repos,
       searchUsers,
       clearUsers,
+      getUser,
+      getUserRepos,
     }}>
       {children}
     </GithubContext.Provider>
